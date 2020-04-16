414dae8011bfcdc8542339d557cec8909ed50b05
Alexander Wang <alexander.wang22@gmail.com>
2020-04-15 22:46:33 -0400

    merge conflicts still?

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import styled from "@emotion/styled";
-import Home from "./Home";
-import Login from "./Login";
-import SignUp from "./SignUp";
-import Header from "./Header";
-import PairGenerator from "./PairGenerator";
-import UserPage from "./UserPage";
-import Format from "./Format";
-import Complexity from "./Complexity";
-import Behavioral from "./Behavioral";
-import Archive from "./Archive";
-import { useTheme } from "./ThemeContext";
-import { AuthProvider } from "./Auth";
-import { firebase } from "../config/firebase.js";
-import { Button, Grid, Link } from "@material-ui/core";
-import mymui from "../mymui.png";
-import Brightness4Icon from "@material-ui/icons/Brightness4";
-import Brightness4OutlinedIcon from "@material-ui/icons/Brightness4Outlined";
-=======
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import styled from '@emotion/styled';
@@ -32,6 +10,7 @@ import UserPage from './UserPage';
 import Format from './Format';
 import Complexity from './Complexity';
 import Behavioral from './Behavioral';
+import Archive from "./Archive";
 import SortingVisualizer from './SortingVisualizer';
 import { useTheme } from './ThemeContext';
 import { AuthProvider } from './Auth';
@@ -40,7 +19,6 @@ import { Button, Grid, Link } from '@material-ui/core';
 import mymui from '../mymui.png';
 import Brightness4Icon from '@material-ui/icons/Brightness4';
 import Brightness4OutlinedIcon from '@material-ui/icons/Brightness4Outlined';
->>>>>>> 51a19bf7a1f5fd7134376f1fb07c971bbee6e779
 
 const Wrapper = styled('div')`
   background: ${(props) => props.theme.background};
@@ -72,7 +50,6 @@ const App = () => {
         <BrowserRouter>
           <Header user={user} />
           <Switch>
-<<<<<<< HEAD
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={SignUp} />
             <Route exact path="/format" component={Format} />
@@ -81,14 +58,6 @@ const App = () => {
             <Route exact path="/pairgenerator" component={PairGenerator} />
             <Route exact path="/archive" component={Archive} />
             <Route exact path="/" component={() => <Home user={user} />} />
-=======
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/signup' component={SignUp} />
-            <Route exact path='/format' component={Format} />
-            <Route exact path='/complexity' component={Complexity} />
-            <Route exact path='/behavioral' component={Behavioral} />
-            <Route exact path='/pairgenerator' component={PairGenerator} />
->>>>>>> 51a19bf7a1f5fd7134376f1fb07c971bbee6e779
             <Route
               exact
               path='/sortingVisualizer'
